0bd904eac9dbb7ef9eb65eccf78a7b3f8a7f720e
othniel-agera <othniel.agera@flutterwavego.com>
2023-01-18 13:02:00 +0100

    Delete Build

--- build/controllers/APIController.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// import "./API/UserController";
-require("./API/ProductController");
-require("./API/OrderController");
-require("./API/CartController");
-//# sourceMappingURL=APIController.js.map
